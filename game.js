588d599e1282f3c29444730f1d6ff8634abc1404
colorspray <colorspraylu@gmail.com>
2014-08-01 20:42:36 +0800

    Super enemy

--- game.js
+++ game.js
@@ -6,7 +6,10 @@ BasicGame.Game.prototype = {
     preload: function () {
         this.load.image('sea', 'assets/sea.png');
         this.load.image('bullet', 'assets/bullet.png');
+        this.load.image('enemyBullet', 'assets/enemy-bullet.png');
+        this.load.image('powerup1', 'assets/powerup1.png');
         this.load.spritesheet('greenEnemy', 'assets/enemy.png', 32, 32);
+        this.load.spritesheet('whiteEnemy', 'assets/shooting-enemy.png', 32, 32);
         this.load.spritesheet('explosion', 'assets/explosion.png', 32, 32);
         this.load.spritesheet('player', 'assets/player.png', 64, 64);
     },
@@ -18,6 +21,7 @@ BasicGame.Game.prototype = {
         this.setupEnemies();
         this.setupBullets();
         this.setupExplosions();
+        this.setupPlayerIcons();
         this.setupText();
 
         this.cursors = this.input.keyboard.createCursorKeys();
@@ -27,14 +31,27 @@ BasicGame.Game.prototype = {
         this.sea.tilePosition.y += 0.2;
         this.processPlayerInput();
         this.spawnEnemies();
+        this.enemyFire();
         this.checkCollisions();
         this.processDelayedEffects();
+    },
+
+    enemyFire:function(){
+        this.shooterPool.forEachAlive(function (enemy){
+            if (this.time.now > enemy.nextShotAt && this.enemyBulletPool.countDead() > 0){
+                var bullet = this.enemyBulletPool.getFirstExists(false);
+                bullet.reset(enemy.x, enemy.y);
+                this.physics.arcade.moveToObject(bullet, this.player, 150);
+                enemy.nextShotAt = this.time.now + 2000;
+            }
+        }, this);
     },
 
     setupPlayer: function () {
         this.player = this.add.sprite(400, 650, 'player');
         this.player.anchor.setTo(0.5, 0.5);
         this.player.animations.add('fly', [0, 1, 2], 20, true);
+        this.player.animations.add('ghost', [3, 0, 3, 1], 20, true);
         this.player.play('fly');
         this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
         this.player.speed = 300;
@@ -61,6 +78,26 @@ BasicGame.Game.prototype = {
 
         this.nextEnemyAt = 0;
         this.enemyDelay = 1000;
+
+        this.shooterPool = this.add.group();
+        this.shooterPool.enableBody = true;
+        this.shooterPool.physicsBodyType = Phaser.Physics.ARCADE;
+        this.shooterPool.createMultiple(20, 'whiteEnemy');
+        this.shooterPool.setAll('anchor.x', 0.5);
+        this.shooterPool.setAll('anchor.y', 0.5);
+        this.shooterPool.setAll('outOfBoundsKill', true);
+        this.shooterPool.setAll('checkWorldBounds', true);
+
+        this.shooterPool.forEach(function(enemy){
+            enemy.animations.add('fly', [0, 1, 2], 20, true);
+            enemy.animations.add('hit', [3, 1, 3, 2], 20, false);
+            enemy.events.onAnimationComplete.add(function(e){
+                e.play('fly')
+            }, this);
+        });
+
+        this.nextShooterAt = this.time.now + 5000;
+        this.shooterDelay = 3000;
     },
 
     setupBullets: function () {
@@ -75,6 +112,16 @@ BasicGame.Game.prototype = {
 
         this.nextShotAt = 0;
         this.shotDelay = 500;
+
+        this.enemyBulletPool = this.add.group();
+        this.enemyBulletPool.enableBody = true;
+        this.enemyBulletPool.physicsBodyType = Phaser.Physics.ARCADE;
+        this.enemyBulletPool.createMultiple(100, 'enemyBullet');
+        this.enemyBulletPool.setAll('anchor.x', 0.5);
+        this.enemyBulletPool.setAll('anchor.y', 0.5);
+        this.enemyBulletPool.setAll('outOfBoundsKill', true);
+        this.enemyBulletPool.setAll('checkWorldBounds', true);
+
     },
 
     setupExplosions: function () {
@@ -96,7 +143,14 @@ BasicGame.Game.prototype = {
             {font: '20px monospace', fill: '#fff', align: 'center'}
         );
         this.instructions.anchor.setTo(0.5, 0.5);
-        this.instExpire = this.time.now + 1000;
+        this.instExpire = this.time.now + 10000;
+
+        this.score = 0;
+        this.scoreText = this.add.text(
+            510, 30, '' + this.score,
+            {font:'20px monospace', fill:'#fff', align:'center'}
+        );
+        this.scoreText.anchor.setTo(0.5, 0.5);
     },
 
     checkCollisions:function(){
@@ -105,7 +159,19 @@ BasicGame.Game.prototype = {
         );
 
         this.physics.arcade.overlap(
+            this.bulletPool, this.shooterPool, this.enemyHit, null, this
+        );
+
+        this.physics.arcade.overlap(
             this.player, this.enemyPool, this.playerHit, null, this
+        );
+
+        this.physics.arcade.overlap(
+            this.player, this.shooterPool, this.playerHit, null, this
+        );
+
+        this.physics.arcade.overlap(
+            this.player, this.enemyBulletPool, this.playerHit, null, this
         );
     },
 
@@ -117,6 +183,20 @@ BasicGame.Game.prototype = {
             enemy.body.velocity.y = this.rnd.integerInRange(30, 60);
             enemy.play('fly');
             enemy.health = 2;
+        }
+
+        if (this.nextShooterAt < this.time.now && this.shooterPool.countDead() > 0){
+            this.nextShooterAt = this.time.now + this.shooterDelay;
+            var enemy = this.shooterPool.getFirstExists(false);
+
+            enemy.reset(this.rnd.integerInRange(20, 1004), 0);
+            var target = this.rnd.integerInRange(20, 1004);
+            enemy.rotation = this.physics.arcade.moveToXY(enemy, target, 768,
+            this.rnd.integerInRange(30, 80)) - Math.PI / 2;
+
+            enemy.play('fly');
+            enemy.health = 5;
+            enemy.nextShotAt = 0;
         }
     },
 
@@ -145,13 +225,33 @@ BasicGame.Game.prototype = {
 
         if (this.input.keyboard.isDown(Phaser.Keyboard.Z) ||
             this.input.activePointer.isDown) {
-            this.fire();
+            if (this.returnText && this.returnText.exists){
+                this.quitGame();
+            }
+            else{
+                this.fire();
+            }
         }
     },
 
     processDelayedEffects:function(){
         if (this.instructions.exists && this.time.now > this.instExpire) {
             this.instructions.destroy();
+        }
+
+        if (this.ghostUntil && this.ghostUntil < this.time.now){
+            this.ghostUntil = null;
+            this.player.play('fly');
+        }
+
+        if (this.showReturn && this.time.now > this.showReturn){
+            this.returnText = this.add.text(
+                512, 400,
+                'Press Z or Tap Game to go back to Main Menu',
+                {font:"16px sans-serif", fill:"#fff"}
+            );
+            this.returnText.anchor.setTo(0.5, 0.5);
+            this.showReturn = false;
         }
     },
 
@@ -160,9 +260,21 @@ BasicGame.Game.prototype = {
     },
 
     playerHit: function (player, enemy) {
+        if (this.ghostUntil && this.ghostUntil > this.time.now){
+            return;
+        }
         this.damageEnemy(enemy, 5);
-        this.explode(player);
-        player.kill();
+        var life = this.lives.getFirstAlive();
+        if (life){
+            life.kill();
+            this.ghostUntil = this.time.now + 3000;
+            this.player.play('ghost');
+        }
+        else{
+            this.explode(player);
+            player.kill();
+            this.displayEnd(false);
+        }
     },
 
     fire: function () {
@@ -190,6 +302,15 @@ BasicGame.Game.prototype = {
         //  Stop music, delete sprites, purge caches, free resources, all that good stuff.
 
         //  Then let's go back to the main menu.
+
+        this.sea.destroy();
+        this.player.destroy();
+        this.enemyPool.destroy();
+        this.bulletPool.destroy();
+        this.explosionPool.destroy();
+        this.instructions.destroy();
+        this.endText.destroy();
+        this.returnText.destroy();
         this.state.start('MainMenu');
 
     },
@@ -218,6 +339,38 @@ BasicGame.Game.prototype = {
         }
         else{
             this.explode(enemy);
+            if (enemy.key === "greenEnemy"){
+                this.score += 100;
+            }
+            else if (enemy.key === "whiteEnemy"){
+                this.score += 400;
+            }
+            this.scoreText.text = this.score;
+            if (this.score >= 2000){
+                this.enemyPool.destroy();
+                this.shooterPool.destroy();
+                this.enemyBulletPool.destroy();
+                this.displayEnd(true);
+            }
         }
+    },
+
+    setupPlayerIcons:function(){
+        this.lives = this.add.group();
+        for (var i = 0; i < 3; i++){
+            var life = this.lives.create(924 + (30 * i), 30, 'player');
+            life.scale.setTo(0.5, 0.5);
+            life.anchor.setTo(0.5, 0.5);
+        }
+    },
+
+    displayEnd:function(win){
+        var msg = win ? 'You win!!!' : 'GameOver!';
+        this.endText = this.add.text(
+            510, 320, msg,
+            {font:"72px serif", fill:"fff"}
+        );
+        this.endText.anchor.setTo(0.5, 0);
+        this.showReturn = this.time.now + 2000;
     }
 };
--- mainMenu.js
+++ mainMenu.js
@@ -7,18 +7,11 @@ BasicGame.MainMenu = function (game) {
 
 BasicGame.MainMenu.prototype = {
 
+    preload:function(){
+        this.load.image('titlepage', 'assets/titlepage.png');
+    },
+
     create: function () {
-
-        //  We've already preloaded our assets, so let's kick right into the Main Menu itself.
-        //  Here all we're doing is playing some music and adding a picture and button
-        //  Naturally I expect you to do something significantly better :)
-
-        this.add.sprite(0, 0, 'titlepage');
-
-        this.loadingText = this.add.text(510, 520, "Press Z or tap/click game to start", { font: "24px monospace", fill: "#fff" });
-        this.loadingText.anchor.setTo(0.5, 0.5);
-        this.add.text(510, 680, "image assets Copyright (c) 2002 Ari Feldman", { font: "16px monospace", fill: "#fff", align: "center"}).anchor.setTo(0.5, 0.5);
-        this.add.text(510, 700, "sound assets Copyright (c) 2012 - 2013 Devin Watson", { font: "16px monospace", fill: "#fff", align: "center"}).anchor.setTo(0.5, 0.5);
 
     },
 
